96409ea59d657e103ac3eb025825d5db0629c9b1
Onesimo Hernandez <ecbeyond@hotmail.com>
2022-07-17 12:49:42 -0400

    Second Commit

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from catfish_first_part import catfich_1986_2001
+
 import pandas as pd
 from datetime import date, datetime
+from prepare_data import get_train_test_data
+from train_execute import train_test
 
-catfich_1986_2001(False)
+train, test = get_train_test_data()
+train_test(train, test)
+
+# try:
+#     db = pd.read_csv('Data/train.csv') 
+# except:
+#     i = 1
+# db_last = db.iloc[-1:]
+i  = 1
+#catfich_1986_2001(False)
 
 
